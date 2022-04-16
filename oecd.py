7f57d4b0f7233fe4a3d721264e5418d28351694c
aloenne-qery <alf.lonne@qery.no>
2022-04-16 16:00:01 +0200

    new figure

--- oecd.py
+++ oecd.py
@@ -9,5 +9,23 @@ import pandas as pd
 os.makedirs('data', exist_ok=True)
 access_token = os.getenv('DW_TOKEN')
 
-#xxxxx
+#ODA Grant Equivalent Measure, Total from 1960 (69Npy)
+oecd_url='https://stats.oecd.org/SDMX-JSON/data/TABLE1/20001.1.11010.1160.D/all?startTime=2018&pid=c0dcdd50-2d08-440b-94d7-8aa50471b7ff'
+resultat = requests.get(oecd_url, headers={'Accept': 'text/csv'})
+df=pd.read_csv(io.StringIO(resultat.text))
+df_new = df.pivot(index='Year', columns='Donor', values='Value')
+oecd_url='https://stats.oecd.org/SDMX-JSON/data/TABLE1/20001.1.1010.1140.D/all?startTime=1960&endTime=2017&dimensionAtObservation=allDimensions&pid=c0dcdd50-2d08-440b-94d7-8aa50471b7ff'
+resultat = requests.get(oecd_url, headers={'Accept': 'text/csv'})
+df_pre18=pd.read_csv(io.StringIO(resultat.text))
+df_new_pre18 = df_pre18.pivot(index='Year', columns='Donor', values='Value')
+df_new_1 = pd.concat([df_new_pre18,df_new])
+df_new_1.to_csv('data/OECD_ODA_Total.csv', index=True)
 
+#Update DW
+chartid = '69Npy'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*"
+    }
+response = requests.request("POST", url, headers=headers)
