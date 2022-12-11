97072d8c45dd3e4b54922e27f288d0a56f218e96
aloenne-qery <alf.lonne@qery.no>
2022-12-11 22:34:35 +0100

    test http

--- oecd_test.py
+++ oecd_test.py
@@ -10,6 +10,7 @@ os.makedirs('data', exist_ok=True)
 access_token = os.getenv('DW_TOKEN')
 
 #ODA Grant Equivalent Measure, Total from 1960 (69Npy)
-oecd_url='https://stats.oecd.org/SDMX-JSON/data/TABLE1/20001.1.11010.1160.D/all?startTime=2018'
+oecd_url='http://stats.oecd.org/SDMX-JSON/data/TABLE1/20001.1.11010.1160.D/all?startTime=2018'
 response = requests.get(oecd_url)
 data=response.json()
+
